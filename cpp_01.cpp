bc3b8a674b43d80ef036725f223fac8758ee5ead
parksungsu <parkss1324@naver.com>
2024-10-28 10:13:52 +0900

    cpp 수정 테스트

--- cpp_01.cpp
+++ cpp_01.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
+#include <iostream> // io(input output)stream이라는 헤더를 포함(include)
 
-using namespace std;
+using namespace std; // 네임스페이스 설명 std::을 생략하게 함
 
 int main() {
+
     cout << "hello world\n" << endl;
 
     return 0;
