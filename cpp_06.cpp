82878781f2247ac4a2d2277641e455c16141fcaf
parksungsu <parkss1324@naver.com>
2024-12-10 11:53:56 +0900

    new, delete

--- cpp_06.cpp
+++ cpp_06.cpp
@@ -1,16 +1,30 @@
 #include <iostream> 
 
-using namespace std; 
-
 int main() {
 
-    int a = 123; // a라는 메모리 주소에서 123이라는 값을 저장
-    
-    int *b = &a; // b에 a의 메모리 주소 대입 
+    int *pData = new int; // 인스턴스만 동적으로 생성하는 경우
 
-    *b = 567; // b가 가르키고 있는 주소에 저장되어 있는 값을 변경시킴
+    int *pNewData = new int(10); // 초기값을 기술하는 경우
 
-    cout << a << " " << b << " " << *b << endl;
+    *pData = 5;
+
+    std::cout << *pData << std::endl;
+    std::cout << *pNewData << std::endl;
+
+    delete pData;
+    delete pNewData;
+
+    int *arr = new int[5]; // 객체를 배열 형태로 동적 생성
+
+    for(int i = 0; i < 5; ++i) {
+        arr[i] = (i + 1) * 10;
+    }
+
+    for(int i = 0; i < 5; ++i) {
+        std::cout << arr[i] << std::endl;
+    }
+
+    delete[] arr; // 배열 형태로 생성한 대상은 반드시 배열 형태로 삭제
 
     return 0;
 }
