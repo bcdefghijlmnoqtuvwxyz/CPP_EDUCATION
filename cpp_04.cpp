f7a70da4859869f2fc6cb255a5ed232259619619
parksungsu <parkss1324@naver.com>
2024-12-10 11:29:37 +0900

    cin

--- cpp_04.cpp
+++ cpp_04.cpp
@@ -1,19 +1,23 @@
+#include <string>
 #include <iostream> 
-
-using namespace std; 
 
 int main() {
 
-    char user_input[100];
-    int number = -1;
-    
-    //cin >> number;
-    //cin.getline(user_input, sizeof(user_input)); // 빈 칸 이후의 단어도 그대로 출력하게 함
-    cin.ignore(100, '\n'); // 100글자 이상 또는 줄바꿈 문자가 있으면 무시하겠다는 뜻
+    int nAge;
+    std::cout << "나이를 입력하세요." << std::endl;
+    std::cin >> nAge;
 
-    cin >> number;
+    char szJob[32];
+    std::cout << "직업을 입력하세요." << std::endl;
+    std::cin >> szJob;
 
-    cout << user_input << " " << number << endl;
+    std::string strName;
+    std::cout << "이름을 입력하세요." << std::endl;
+    std::cin >> strName;
+
+    std::cout << "당신의 이름은 " << strName << "이고, "
+        << "나이는 " << nAge << "살이며, "
+        << "직업은 " << szJob <<"입니다. " << std::endl;
 
     return 0;
 }
