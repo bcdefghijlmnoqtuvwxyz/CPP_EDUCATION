083fb54c5cdae68e7022f8654b939cf17f8db069
parksungsu <parkss1324@naver.com>
2024-12-10 17:32:40 +0900

    디폴트 매개변수

--- cpp_08.cpp
+++ cpp_08.cpp
@@ -2,27 +2,16 @@
 
 using namespace std; 
 
-int Add(int, int);
-
-void Add(int a. int b, int *c, int *d) { // 반환값을 여러개 받을 때 포인터를 믾이 사용
-    *c = a + b;
-    *d = a - b;
+int TestFunc(int nParam, int nParam2 = 2)
+{
+    return nParam * nParam2;
 }
 
-int main() {
+int main() 
+{
+    cout << TestFunc(10) << endl;
 
-    cout << Add(1, 2) << endl;
-
-    int sum;
-    int sub;
-
-    Add(4, 5, &sum, &sub);
-
-    cout << sum << " " << sub << endl;
+    cout << TestFunc(10, 5) << endl; 
 
     return 0;
-}
-
-int Add(int a, int b) {
-    return a + b;
 }
