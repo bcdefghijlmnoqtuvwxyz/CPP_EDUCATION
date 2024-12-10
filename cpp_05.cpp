2206f29da6102f3d3e296d4207f9158f06471dae
parksungsu <parkss1324@naver.com>
2024-12-10 11:46:02 +0900

    auto

--- cpp_05.cpp
+++ cpp_05.cpp
@@ -1,16 +1,12 @@
 #include <iostream> 
 
-using namespace std;
-
 int main() {
 
-    char my_string[] = "Hello, world!";
+    int a = 10;
+    int b(a);
+    auto c(b);
 
-    for(int i = 0; my_string[i] != '\0'; i++) { // 개행 문자 전까지 출력
-        cout << my_string[i];
-    }
-
-    cout << endl;
+    std::cout << a + b + c << std::endl;
 
     return 0;
 }
