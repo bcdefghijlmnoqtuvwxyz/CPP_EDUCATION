5effcddbb2937f6408fe9adefd0bd47e13d6ba3e
parksungsu <parkss1324@naver.com>
2024-12-10 21:40:55 +0900

    인라인 함수

---
+++ cpp_11.cpp
@@ -0,0 +1,27 @@
+#include <iostream> 
+
+#define ADD(a, b)((a) + (b))
+
+using namespace std;
+
+int Add(int a, int b)
+{
+    return a + b;
+}
+
+inline int AddNew(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    printf("ADD(): %d\n", ADD(a, b));
+    printf("Add(): %d\n", Add(a, b));
+    printf("AddNew(): %d", AddNew(a, b));
+
+    return 0;
+}
