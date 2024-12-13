4960f5843c9c36de8c2d8f25723f2016723ae314
parksungsu <parkss1324@naver.com>
2024-12-13 14:59:05 +0900

    정적 멤버 static

--- cpp_28.cpp
+++ cpp_28.cpp
@@ -9,7 +9,7 @@ public:
     int GetData() {return m_nData;};
     void ResetCount() {m_nCount = 0;};
 
-    static int GetCount()
+    static int GetCount() // 정적 메서드 선언 및 정의
     {
         return m_nCount;
     }
@@ -27,9 +27,10 @@ int main()
 {
     CTest a(5), b(10);
 
-    cout << a.GetCount() << endl;
+    cout << a.GetCount() << endl; // 정적 멤버에 접근
     b.ResetCount();
 
+    // 정적 멤버에 접근, 인스턴스 없이도 접근 가능
     cout << CTest::GetCount() << endl;
 
     return 0;
